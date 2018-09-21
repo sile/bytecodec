f2f33c212dced497511267af3dbd58b1dc1a6701
Takeru Ohta <phjgt308@gmail.com>
2018-09-22 00:25:04 +0900

    Use `TrackableError` derive macro

--- src/error.rs
+++ src/error.rs
@@ -3,9 +3,8 @@ use trackable::error::{ErrorKind as TrackableErrorKind, ErrorKindExt};
 use trackable::error::{Failure, TrackableError};
 
 /// This crate specific `Error` type.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, TrackableError)]
 pub struct Error(TrackableError<ErrorKind>);
-derive_traits_for_trackable_error_newtype!(Error, ErrorKind);
 impl From<Failure> for Error {
     fn from(f: Failure) -> Self {
         ErrorKind::Other.takes_over(f).into()
