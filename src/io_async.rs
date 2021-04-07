25a7f110e0f1b7ff5d91f60e7f581085b41e3f3c
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-04-07 16:23:24 +0900

    Translate comments, Poll03 -> Poll

--- src/io_async.rs
+++ src/io_async.rs
@@ -2,7 +2,7 @@
 use crate::io::{BufferedIo, ReadBuf, StreamState, WriteBuf};
 use crate::{Error, Result};
 use core::pin::Pin;
-use core::task::{Context, Poll as Poll03};
+use core::task::{Context, Poll};
 use tokio::io::{AsyncRead, AsyncWrite};
 
 impl<B: AsRef<[u8]> + AsMut<[u8]>> ReadBuf<B> {
@@ -16,30 +16,30 @@ impl<B: AsRef<[u8]> + AsMut<[u8]>> ReadBuf<B> {
         &mut self,
         mut reader: Pin<&mut R>,
         cx: &mut Context<'_>,
-    ) -> Poll03<Result<()>> {
+    ) -> Poll<Result<()>> {
         while !self.is_full() {
             let mut buffer = tokio::io::ReadBuf::new(&mut self.inner.as_mut()[self.tail..]);
             match reader.as_mut().poll_read(cx, &mut buffer) {
-                Poll03::Pending => {
+                Poll::Pending => {
                     self.stream_state = StreamState::WouldBlock;
-                    return Poll03::Pending;
+                    return Poll::Pending;
                 }
-                Poll03::Ready(Ok(())) => {
+                Poll::Ready(Ok(())) => {
                     let size = buffer.filled().len();
                     if size == 0 {
                         self.stream_state = StreamState::Eos;
-                        return Poll03::Ready(Ok(()));
+                        return Poll::Ready(Ok(()));
                     }
                     self.stream_state = StreamState::Normal;
                     self.tail += size;
                 }
-                Poll03::Ready(Err(e)) => {
+                Poll::Ready(Err(e)) => {
                     self.stream_state = StreamState::Error;
-                    return Poll03::Ready(Err(track!(Error::from(e))));
+                    return Poll::Ready(Err(track!(Error::from(e))));
                 }
             }
         }
-        Poll03::Ready(Ok(()))
+        Poll::Ready(Ok(()))
     }
 }
 
@@ -56,22 +56,22 @@ impl<B: AsRef<[u8]> + AsMut<[u8]>> WriteBuf<B> {
         &mut self,
         mut writer: Pin<&mut W>,
         cx: &mut Context<'_>,
-    ) -> Poll03<Result<()>> {
+    ) -> Poll<Result<()>> {
         while !self.is_empty() {
             match writer
                 .as_mut()
                 .poll_write(cx, &self.inner.as_ref()[self.head..self.tail])
             {
-                Poll03::Ready(Err(e)) => {
+                Poll::Ready(Err(e)) => {
                     self.stream_state = StreamState::Error;
-                    return Poll03::Ready(Err(track!(Error::from(e))));
+                    return Poll::Ready(Err(track!(Error::from(e))));
                 }
-                Poll03::Ready(Ok(0)) => {
+                Poll::Ready(Ok(0)) => {
                     self.stream_state = StreamState::Eos;
                     // stream is closed. No need to wake up this future :)
-                    return Poll03::Ready(Ok(()));
+                    return Poll::Ready(Ok(()));
                 }
-                Poll03::Ready(Ok(size)) => {
+                Poll::Ready(Ok(size)) => {
                     self.stream_state = StreamState::Normal;
                     self.head += size;
                     if self.head == self.tail {
@@ -79,15 +79,15 @@ impl<B: AsRef<[u8]> + AsMut<[u8]>> WriteBuf<B> {
                         self.tail = 0;
                     }
                 }
-                Poll03::Pending => {
+                Poll::Pending => {
                     self.stream_state = StreamState::WouldBlock;
-                    return Poll03::Pending;
+                    return Poll::Pending;
                 }
             }
         }
         // Now the buffer is empty. Because the returned value is not Poll::Pending,
         // it is *the caller*'s responsibility to ensure this future is woken up.
-        Poll03::Ready(Ok(()))
+        Poll::Ready(Ok(()))
     }
 }
 
@@ -95,22 +95,25 @@ impl<T: AsyncRead + AsyncWrite> BufferedIo<T> {
     /// Executes an I/O operation on the inner stream.
     ///
     /// "I/O operation" means "filling the read buffer" and "flushing the write buffer".
-    pub fn execute_io_poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll03<Result<()>> {
+    /// This function returns Poll::Pending when both rbuf and wbuf are not ready for I/O operations.
+    pub fn execute_io_poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<()>> {
         let mut this = self.project();
         let rresult = this.rbuf.poll_fill(this.stream.as_mut(), cx);
         let wresult = this.wbuf.poll_flush(this.stream.as_mut(), cx);
-        if let (&Poll03::Pending, &Poll03::Pending) = (&rresult, &wresult) {
-            // rbuf 側と wbuf 側のどちらの準備ができても、この future は呼び起こされる。
-            return Poll03::Pending;
+        if let (&Poll::Pending, &Poll::Pending) = (&rresult, &wresult) {
+            // This future will be polled again when either rbuf or wbuf is ready.
+            return Poll::Pending;
         }
-        if let Poll03::Ready(rresult) = rresult {
+        if let Poll::Ready(rresult) = rresult {
             track!(rresult)?;
         }
-        if let Poll03::Ready(wresult) = wresult {
+        if let Poll::Ready(wresult) = wresult {
             track!(wresult)?;
         }
-        // rbuf 側と wbuf 側のどちらも Poll::Pending を返す状態でないと、この future が呼び起こされる保証がない。
-        // Ready を返して、これの呼び出し側にループなり呼び起こしの確約なりをしてもらう必要がある。
-        Poll03::Ready(Ok(()))
+
+        // If at least one of rbuf or wbuf returns Poll::Ready,
+        // there's no guarantee that the waker is signaled at some point.
+        // Poll::Ready here means it's the caller's responsibility to ensure the waker is signaled later.
+        Poll::Ready(Ok(()))
     }
 }
